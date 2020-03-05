bd5b4c569c094c9b3bdc25ee8913b23e25177b62
hbw <hbw1224@163.com>
2020-03-05 14:37:56 +0800

    初始布局加轮播

---
+++ src/views/home/childComps/RecommendView.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="recommend">
+    <div v-for="(item,index) in recommends" :key="index" class="recommend-item">
+      <a :href="item.link">
+        <img :src="item.image" alt="">
+        <div>{{item.title}}</div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "RecommendView",
+    props: {
+      recommends: {
+        type: Array,
+        default() {
+          return []
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .recommend {
+    display: flex;
+    width: 100%;
+    text-align: center;
+    font-size: 12px;
+
+    padding: 6px 0 10px;
+    border-bottom: 10px solid #eee;
+  }
+
+  .recommend-item {
+    flex: 1;
+  }
+
+  .recommend-item img {
+    width: 70px;
+    height: 70px;
+    margin-bottom: 10px;
+  }
+</style>
